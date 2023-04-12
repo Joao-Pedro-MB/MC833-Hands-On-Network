816d0472c8d7ea6d42235fe6588bc05afdbf04b9
Joao Pedro De Moraes Bonucci <joao.bonucci@ifood.com.br>
2023-04-12 20:21:51 -0300

    added client interface

--- src/client-front.c
+++ src/client-front.c
@@ -1,5 +1,14 @@
 #include <stdio.h>
 
+/*
+• cadastrar um novo perfil utilizando o email como identificador;
+• listar todas as pessoas (email e nome) formadas em um determinado curso;
+• listar todas as pessoas (email e nome) que possuam uma determinada habilidade;
+• listar todas as pessoas (email, nome e curso) formadas em um determinado ano;
+• listar todas as informações de todos os perfis;
+• dado o email de um perfil, retornar suas informações;
+• remover um perfil a partir de seu identificador (email);
+*/
 
 int main () {
     int client_input_int;
@@ -45,22 +54,40 @@ void create_new_user() {
     char client_input[100];
     printf( "Type de new user's email:\n");
     scanf ("%d",&client_input);
+
+    char * request = format_message("create-profile", NULL, NULL, client_input);
+}
+
+void show_all_profiles() {
+    char * request = format_message("show-all", NULL, NULL,  NULL, NULL);
 }
 
 void search_group_of_profiles() {
     char field[100], comparison_method[100], value[100];
     printf( "Type the field, comparison method(greater than, less than, equal, different, contains, excludes) and value devided by commas (ex: Age, greater than, 29):\n");
     scanf ("%s,%s,%s",&field, &comparison_method, &value);
+
+    char * request = format_message("search-batch", field, comparison_method, value);
 }
 
 void find_profile() {
     char client_input[100];
     printf( "Type de new user's email:\n");
     scanf ("%d",&client_input);
+
+    char * request = format_message("search-profile", NULL, NULL, client_input);
 }
 
 void delete_profile() {
     char client_input[100];
     printf( "Type de new user's email:\n");
     scanf ("%d",&client_input);
+
+    char * request = format_message("delete-profile", NULL, NULL, client_input);
+}
+
+char * format_message(char * Command, char * Field, char * Comparison_method, char * Value) {
+    char * message = malloc(1000);
+    sprintf(message, "{\"Command\": %s, \"Field\": %s, \"Comparison_Method\": %s, \"Value\": %s}", action, field, comparison_method, value);
+    return message;
 }
--- src/client-socket.c
+++ src/client-socket.c
@@ -19,8 +19,7 @@
 #define MAXDATASIZE 1000 // max number of bytes we can get at once 
 
 // get sockaddr, IPv4 or IPv6:
-void *get_in_addr(struct sockaddr *sa)
-{
+void *get_in_addr(struct sockaddr *sa) {
 	if (sa->sa_family == AF_INET) {
 		return &(((struct sockaddr_in*)sa)->sin_addr);
 	}
@@ -28,19 +27,7 @@ void *get_in_addr(struct sockaddr *sa)
 	return &(((struct sockaddr_in6*)sa)->sin6_addr);
 }
 
-int main(int argc, char *argv[])
-{
-	int sockfd, bytes_received;  
-	char buf[MAXDATASIZE];
-	struct addrinfo hints, *servinfo, *p;
-	int rv;
-	char s[INET6_ADDRSTRLEN];
-
-	if (argc != 2) {
-		fprintf(stderr,"usage: client hostname\n");
-		exit(1);
-	}
-
+int start_and_connect(int * sockfd, addrinfo * hints, servinfo, p, int * rv, char * s){
 	memset(&hints, 0, sizeof hints);
 	hints.ai_family = AF_UNSPEC;
 	hints.ai_socktype = SOCK_STREAM;
@@ -52,8 +39,7 @@ int main(int argc, char *argv[])
 
 	// loop through all the results and connect to the first we can
 	for(p = servinfo; p != NULL; p = p->ai_next) {
-		if ((sockfd = socket(p->ai_family, p->ai_socktype,
-				p->ai_protocol)) == -1) {
+		if ((sockfd = socket(p->ai_family, p->ai_socktype, p->ai_protocol)) == -1) {
 			perror("client: socket");
 			continue;
 		}
@@ -77,23 +63,38 @@ int main(int argc, char *argv[])
 	printf("client: connecting to %s\n", s);
 
 	freeaddrinfo(servinfo); // all done with this structure
+}
 
-	FILE *fp = fopen("./text/example.json", "r");
-	
-	if (!fp) {
-		printf("Failed to open file\n");
-		return 1;
+int main(int argc, char *argv[]) {
+	int sockfd, bytes_received;  
+	char buf[MAXDATASIZE];
+	struct addrinfo hints, *servinfo, *p;
+	int rv;
+	char s[INET6_ADDRSTRLEN];
+
+	if (argc != 2) {
+		fprintf(stderr,"usage: client hostname\n");
+		exit(1);
 	}
 
-	// Read the JSON data from the file
-	fseek(fp, 0, SEEK_END);
-	long file_size = ftell(fp);
-	fseek(fp, 0, SEEK_SET);
-	char *json_data = malloc(file_size + 1);
-	fread(json_data, 1, file_size, fp);
-	fclose(fp);
+	int err = start_and_connect(&sockfd, &hints, servinfo, p, &rv, s);
 
-	printf("%s\n", json_data);
+	// FILE *fp = fopen("./text/example.json", "r");
+	
+	// if (!fp) {
+	// 	printf("Failed to open file\n");
+	// 	return 1;
+	// }
+
+	// // Read the JSON data from the file
+	// fseek(fp, 0, SEEK_END);
+	// long file_size = ftell(fp);
+	// fseek(fp, 0, SEEK_SET);
+	// char *json_data = malloc(file_size + 1);
+	// fread(json_data, 1, file_size, fp);
+	// fclose(fp);
+
+	// printf("%s\n", json_data);
 
 
 	if (send(sockfd, json_data, strlen(json_data), 0) == -1)
@@ -101,7 +102,7 @@ int main(int argc, char *argv[])
 
 	free(json_data);
 
-	printf("Leu\n");
+
 	bytes_received = recv(sockfd, buf, MAXDATASIZE-1, 0);
 			
 	if (bytes_received == -1) {
