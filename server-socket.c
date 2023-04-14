aabac34a2eebf8d03b737659706c982617a73437
Pedro <pedrodearaujo3001@gmail.com>
2023-04-13 21:29:32 -0300

    fixing server socket

--- server-socket.c
+++ server-socket.c
@@ -1,7 +1,3 @@
-/*
-** server.c -- a stream socket server demo
-*/
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
@@ -14,40 +10,40 @@
 #include <arpa/inet.h>
 #include <sys/wait.h>
 #include <signal.h>
+
 #include "cJSON.h"
 
-#define PORT "3490"  // the port users will be connecting to
+#define PORT "3490"// the port users will be connecting to
+#define BACKLOG 10// how many pending connections queue will hold
 
-#define BACKLOG 10   // how many pending connections queue will hold
+#define MAXDATASIZE 1024 // max number of bytes we can get at once
 
-void sigchld_handler(int s)
-{
+void sigchld_handler(int s) {
     // waitpid() might overwrite errno, so we save and restore it:
     int saved_errno = errno;
-
+    
     while(waitpid(-1, NULL, WNOHANG) > 0);
-
-    errno = saved_errno;
+        errno = saved_errno;
 }
 
-
 // get sockaddr, IPv4 or IPv6:
-void *get_in_addr(struct sockaddr *sa)
-{
+void *get_in_addr(struct sockaddr *sa) {
     if (sa->sa_family == AF_INET) {
         return &(((struct sockaddr_in*)sa)->sin_addr);
     }
-
     return &(((struct sockaddr_in6*)sa)->sin6_addr);
 }
 
-int main(void)
-{
-    int sockfd, new_fd;  // listen on sock_fd, new connection on new_fd
+int main(void) {
+    
+    int sockfd, new_fd; // listen on sock_fd, new connection on new_fd
+    int numbytes;
+    char buf[MAXDATASIZE];
     struct addrinfo hints, *servinfo, *p;
     struct sockaddr_storage their_addr; // connector's address information
     socklen_t sin_size;
     struct sigaction sa;
+
     int yes=1;
     char s[INET6_ADDRSTRLEN];
     int rv;
@@ -61,17 +57,16 @@ int main(void)
         fprintf(stderr, "getaddrinfo: %s\n", gai_strerror(rv));
         return 1;
     }
-
+    
     // loop through all the results and bind to the first we can
     for(p = servinfo; p != NULL; p = p->ai_next) {
-        if ((sockfd = socket(p->ai_family, p->ai_socktype,
-                p->ai_protocol)) == -1) {
+
+        if ((sockfd = socket(p->ai_family, p->ai_socktype, p->ai_protocol)) == -1) {
             perror("server: socket");
             continue;
         }
 
-        if (setsockopt(sockfd, SOL_SOCKET, SO_REUSEADDR, &yes,
-                sizeof(int)) == -1) {
+        if (setsockopt(sockfd, SOL_SOCKET, SO_REUSEADDR, &yes, sizeof(int)) == -1) {
             perror("setsockopt");
             exit(1);
         }
@@ -83,11 +78,12 @@ int main(void)
         }
 
         break;
+
     }
 
     freeaddrinfo(servinfo); // all done with this structure
 
-    if (p == NULL)  {
+    if (p == NULL) {
         fprintf(stderr, "server: failed to bind\n");
         exit(1);
     }
@@ -100,6 +96,7 @@ int main(void)
     sa.sa_handler = sigchld_handler; // reap all dead processes
     sigemptyset(&sa.sa_mask);
     sa.sa_flags = SA_RESTART;
+
     if (sigaction(SIGCHLD, &sa, NULL) == -1) {
         perror("sigaction");
         exit(1);
@@ -107,43 +104,58 @@ int main(void)
 
     printf("server: waiting for connections...\n");
 
-    while(1) {  // main accept() loop
+    while(1) { // main accept() loop
+        
         sin_size = sizeof their_addr;
         new_fd = accept(sockfd, (struct sockaddr *)&their_addr, &sin_size);
+        
         if (new_fd == -1) {
             perror("accept");
             continue;
         }
 
-        inet_ntop(their_addr.ss_family,
-            get_in_addr((struct sockaddr *)&their_addr),
-            s, sizeof s);
+        inet_ntop(their_addr.ss_family, get_in_addr((struct sockaddr *)&their_addr), s, sizeof s);
         printf("server: got connection from %s\n", s);
 
         if (!fork()) { // this is the child process
             close(sockfd); // child doesn't need the listener
 
-            printf("Entrou\n");
+            if ((numbytes = recv(new_fd, buf, MAXDATASIZE - 1, 0)) == -1) {
+                perror("recv");
+                exit(1);
+            }
 
-            char buffer[1024];
-            int bytes_received = recv(sockfd, buffer, sizeof(buffer), 0);
-            
-            buffer[bytes_received] = '\0';
-            
-            cJSON *decoded_root = cJSON_Parse(buffer);
+            cJSON* root = cJSON_Parse(buf);
+            if (root == NULL) {
+                printf("Error parsing JSON: %s\n", cJSON_GetErrorPtr());
+                return 1;
+            }
 
-            char *new_encoded_json = cJSON_Print(decoded_root);
+            buf[numbytes] = '\0';
 
-            printf("%s\n", new_encoded_json);
+            cJSON* operation = cJSON_GetObjectItem(root, "operation");
 
-            // cJSON* name = cJSON_GetObjectItem(decoded_root, "name");
-            // printf("Name: %s\n", name->valuestring);
-            
+            if (operation->valueint == 1) {
+                
+                cJSON* root = cJSON_CreateObject();
+                
+                cJSON_AddStringToObject(root, "name", "John Doe");
+                cJSON_AddNumberToObject(root, "age", 22);
+
+                char* json_str = cJSON_PrintUnformatted(root);
+
+                if (send(new_fd, json_str, strlen(json_str), 0) == -1)
+                    perror("send");
+            }
+    
             close(new_fd);
             exit(0);
         }
-        close(new_fd);  // parent doesn't need this
+
+    close(new_fd); // parent doesn't need this
+
     }
 
     return 0;
-}
+
+}        
