6474504b0f4c456589ab50389a0a70e4cb1773ff
Pedro <pedrodearaujo3001@gmail.com>
2023-05-01 19:22:49 -0300

    minor fixes

--- client/src/client-socket.h
+++ client/src/client-socket.h
@@ -15,7 +15,7 @@
 
 #define IP "127.0.0.1" // the fixed IP that the server is running on
 #define PORT "3490" // the port client will be connecting to
-#define MAXDATASIZE 1000 // max number of bytes we can get at once
+#define MAXDATASIZE 10000 // max number of bytes we can get at once
 
 #define CREATE_PROFILE 1
 #define SEARCH_BATCH 2
